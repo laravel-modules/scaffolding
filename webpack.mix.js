7c6f20e9a9a3b6b1c3854036a545aebdb11d5eb8
Ahmed Fathy <aliraqi.dev@gmail.com>
2021-01-07 21:00:57 +0200

    Bump laravel-mix version to ^6.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,28 @@
 const mix = require('laravel-mix');
-require('laravel-mix-merge-manifest');
-mix.mergeManifest();
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+const WebpackRTLPlugin = require('webpack-rtl-plugin');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// Adminlte3 Template
+mix.js([
+  'resources/js/adminlte3/adminlte3.js',
+  'resources/js/adminlte3/adminlte3-auth.js'
+], 'public/js').vue();
+mix.sass('resources/sass/adminlte3/adminlte3.scss', 'public/css');
+
+// Vali Template
+mix.js('resources/js/vali/vali.js', 'public/js').vue();
+mix.sass('resources/sass/vali/vali.scss', 'public/css');
+
+// Handle rtl
+mix.webpackConfig({
+  plugins: [
+    new WebpackRTLPlugin({
+      diffOnly: false,
+      minify: true,
+    }),
+  ],
+});
+
+mix.version([
+  'public/js/*',
+  'public/css/*',
+]);
