67850b635f92ce428cd9ae87d83fadc4bcbb304b
Ahmed Fathy <aliraqi.dev@gmail.com>
2021-01-16 19:02:07 +0200

    sort generated sidebars

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,5 +6,5 @@
 @endcomponent
 
 @include('dashboard.accounts.sidebar')
-@include('dashboard.settings.sidebar')
 {{-- The sidebar of generated crud will set here: Don't remove this line --}}
+@include('dashboard.settings.sidebar')
