3d4af001a3fdd655f17327dfecdc39af249a935a
Ahmed Fathy <aliraqi.dev@gmail.com>
2021-01-28 19:22:07 +0200

    Update ide helpers

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -10,49 +10,250 @@
  */
 
 
-namespace App{
+namespace App\Models{
 /**
- * App\Media
+ * App\Models\Admin
  *
  * @property int $id
- * @property string $model_type
- * @property int $model_id
- * @property string $collection_name
  * @property string $name
- * @property string $file_name
- * @property string|null $mime_type
- * @property string $disk
- * @property int $size
- * @property array $manipulations
- * @property array $custom_properties
- * @property array $responsive_images
- * @property int|null $order_column
+ * @property string|null $type
+ * @property string|null $email
+ * @property string|null $phone
+ * @property string|null $firebase_id
+ * @property string|null $phone_verified_at
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read string $extension
- * @property-read string $human_readable_size
- * @property-read string $type
- * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $model
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\MediaLibrary\Models\Media ordered()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereCollectionName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereCustomProperties($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereDisk($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereFileName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereManipulations($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereMimeType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereModelId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereModelType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereOrderColumn($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereResponsiveImages($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereSize($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Media whereUpdatedAt($value)
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection|\Spatie\MediaLibrary\MediaCollections\Models\Media[] $media
+ * @property-read int|null $media_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User filter(?\App\Http\Filters\BaseFilters $filters = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User sortingByIds($ids)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereFirebaseId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin wherePhoneVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereUpdatedAt($value)
  */
-	class Media extends \Eloquent {}
+	class Admin extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Customer
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $type
+ * @property string|null $email
+ * @property string|null $phone
+ * @property string|null $firebase_id
+ * @property string|null $phone_verified_at
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection|\Spatie\MediaLibrary\MediaCollections\Models\Media[] $media
+ * @property-read int|null $media_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User filter(?\App\Http\Filters\BaseFilters $filters = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User sortingByIds($ids)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereFirebaseId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer wherePhoneVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereUpdatedAt($value)
+ */
+	class Customer extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Feedback
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property string|null $phone
+ * @property string|null $email
+ * @property string|null $message
+ * @property \Illuminate\Support\Carbon|null $read_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback filter(?\App\Http\Filters\BaseFilters $filters = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback sortingByIds($ids)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback unread()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereMessage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereReadAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feedback whereUpdatedAt($value)
+ */
+	class Feedback extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\ResetPasswordCode
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $code
+ * @property \Illuminate\Support\Carbon $created_at
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordCode newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordCode newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordCode query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordCode whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordCode whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordCode whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordCode whereUsername($value)
+ */
+	class ResetPasswordCode extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\ResetPasswordToken
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $token
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordToken newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordToken newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordToken query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordToken whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordToken whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordToken whereToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ResetPasswordToken whereUserId($value)
+ */
+	class ResetPasswordToken extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Setting
+ *
+ * @property int $id
+ * @property string $key
+ * @property string|null $locale
+ * @property string|null $value
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection|\Spatie\MediaLibrary\MediaCollections\Models\Media[] $media
+ * @property-read int|null $media_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting whereKey($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting whereLocale($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Setting whereValue($value)
+ */
+	class Setting extends \Eloquent implements \Spatie\MediaLibrary\HasMedia {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $type
+ * @property string|null $email
+ * @property string|null $phone
+ * @property string|null $firebase_id
+ * @property string|null $phone_verified_at
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection|\Spatie\MediaLibrary\MediaCollections\Models\Media[] $media
+ * @property-read int|null $media_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User filter(?\App\Http\Filters\BaseFilters $filters = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User sortingByIds($ids)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirebaseId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePhoneVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent implements \Spatie\MediaLibrary\HasMedia {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Verification
+ *
+ * @property \Carbon\Carbon $created_at
+ * @property int $id
+ * @property int $user_id
+ * @property string $phone
+ * @property string $code
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Verification whereUserId($value)
+ */
+	class Verification extends \Eloquent {}
 }
 
