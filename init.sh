81457bd1bbb6b670b80b1216674ffc7bcdd27c08
Ahmed Fathy <aliraqi.dev@gmail.com>
2021-02-02 02:47:43 +0200

    add init.sh

---
+++ init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Initialize the project's environment.
+# This script only works with valet
+
+# Set the project's name from directory.
+PROJECT_NAME=$(basename $(pwd))
+
+# Let user write his username and password of database.
+read -p "Enter the DB_USERNAME: [root]" DB_USERNAME
+read -sp "Enter the DB_PASSWORD: " DB_PASSWORD
+DB_USERNAME=${DB_USERNAME:-root}
+
+echo -e "\n"
+
+# Create the mysql database.
+mysql -u root --password=$DB_PASSWORD -e "create database $PROJECT_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+
+# Create the project's .env file.
+sed -e "s|DB_DATABASE=laravel|DB_DATABASE=$PROJECT_NAME|"\
+    -e "s|DB_USERNAME=root|DB_USERNAME=$DB_USERNAME|"\
+    -e "s|DB_PASSWORD=|DB_PASSWORD=$DB_PASSWORD|"\
+    -e "s|APP_DOMAIN=localhost|APP_DOMAIN=$PROJECT_NAME.test|" ./.env.example > ./.env
+
+# Install dependencies.
+composer install
+
+# Generate the app key.
+php artisan key:generate
+
+# Create symlink for storage and media files.
+php artisan storage:link
+
+# Migrate the tables and seed accounts and dummy data.
+php artisan migrate:fresh --seed
+
+# Print the project's URL.
+echo APP_URL: http://$PROJECT_NAME.test
