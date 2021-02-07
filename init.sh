49635b8f3b781722006a69b763782e8b5fc7a1ce
Ahmed Fathy <aliraqi.dev@gmail.com>
2021-02-07 17:08:39 +0200

    add ability to change APP_DOMAIN

--- init.sh
+++ init.sh
@@ -5,10 +5,12 @@
 # Set the project's name from directory.
 PROJECT_NAME=$(basename $(pwd))
 
-# Let user write his username and password of database.
+# Let user write his domain and the database's username & password.
+read -p "Enter the APP_DOMAIN: [$PROJECT_NAME.test]" APP_DOMAIN
 read -p "Enter the DB_USERNAME: [root]" DB_USERNAME
 read -sp "Enter the DB_PASSWORD: " DB_PASSWORD
 DB_USERNAME=${DB_USERNAME:-root}
+APP_DOMAIN=${APP_DOMAIN:-$PROJECT_NAME.test}
 
 echo -e "\n"
 
@@ -19,7 +21,7 @@ mysql -u root --password=$DB_PASSWORD -e "create database $PROJECT_NAME CHARACTE
 sed -e "s|DB_DATABASE=laravel|DB_DATABASE=$PROJECT_NAME|"\
     -e "s|DB_USERNAME=root|DB_USERNAME=$DB_USERNAME|"\
     -e "s|DB_PASSWORD=|DB_PASSWORD=$DB_PASSWORD|"\
-    -e "s|APP_DOMAIN=localhost|APP_DOMAIN=$PROJECT_NAME.test|" ./.env.example > ./.env
+    -e "s|APP_DOMAIN=localhost|APP_DOMAIN=$APP_DOMAIN|" ./.env.example > ./.env
 
 # Install dependencies.
 composer install
