f3edf19125a8a8fce0e60af5df06858d78a4ac13
Ahmed Fathy <aliraqi.dev@gmail.com>
2021-03-10 17:08:05 +0200

    bind only soft deletable models

--- app/Providers/SoftDeletesServiceProvider.php
+++ app/Providers/SoftDeletesServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SoftDeletesServiceProvider extends ServiceProvider
 {
@@ -17,10 +18,25 @@ class SoftDeletesServiceProvider extends ServiceProvider
         $mapping = json_decode(file_get_contents(storage_path('soft_deletes_route_binding.json')), true);
 
         foreach ($mapping as $key => $class) {
+            if (! $this->hasSoftDeletes($class)) {
+                continue;
+            }
+
             Route::bind($key, function ($value) use ($class) {
                 return $class::withTrashed()->findOrFail($value);
             });
         }
-
+    }
+    /**
+     * Determine wither the model use soft deleting trait.
+     *
+     * @return bool
+     */
+    public function hasSoftDeletes($class)
+    {
+        return in_array(
+            SoftDeletes::class,
+            array_keys((new \ReflectionClass($class))->getTraits())
+        );
     }
 }
