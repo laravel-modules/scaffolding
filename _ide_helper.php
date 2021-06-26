7dc9d894bd4762ae4cac813ee07ed318310fde51
Ahmed Fathy <aliraqi.dev@gmail.com>
2021-06-26 03:08:59 +0200

    update packages

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 8.48.1.
+ * Generated for Laravel 8.48.2.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
