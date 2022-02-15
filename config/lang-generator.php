d2e104ecfa77d031bb56b2dfe7edd308a51de0a6
Ahmed Fathy <aliraqi.dev@gmail.com>
2022-02-15 13:53:35 +0200

    change lang direction

--- config/lang-generator.php
+++ config/lang-generator.php
@@ -8,27 +8,27 @@ return [
      */
 
     'lang' => [
-        'auth' => resource_path('lang/{lang}/auth.php'),
-        'pagination' => resource_path('lang/{lang}/pagination.php'),
-        'passwords' => resource_path('lang/{lang}/passwords.php'),
-        'validation' => resource_path('lang/{lang}/validation.php'),
-        'admins' => resource_path('lang/{lang}/admins.php'),
-        'backup' => resource_path('lang/{lang}/backup.php'),
-        'check-all' => resource_path('lang/{lang}/check-all.php'),
-        'customers' => resource_path('lang/{lang}/customers.php'),
-        'dashboard' => resource_path('lang/{lang}/dashboard.php'),
-        'feedback' => resource_path('lang/{lang}/feedback.php'),
-        'permissions' => resource_path('lang/{lang}/permissions.php'),
-        'select2' => resource_path('lang/{lang}/select2.php'),
-        'settings' => resource_path('lang/{lang}/settings.php'),
-        'supervisors' => resource_path('lang/{lang}/supervisors.php'),
-        'users' => resource_path('lang/{lang}/users.php'),
-        'verification' => resource_path('lang/{lang}/verification.php'),
+        'auth' => base_path('lang/{lang}/auth.php'),
+        'pagination' => base_path('lang/{lang}/pagination.php'),
+        'passwords' => base_path('lang/{lang}/passwords.php'),
+        'validation' => base_path('lang/{lang}/validation.php'),
+        'admins' => base_path('lang/{lang}/admins.php'),
+        'backup' => base_path('lang/{lang}/backup.php'),
+        'check-all' => base_path('lang/{lang}/check-all.php'),
+        'customers' => base_path('lang/{lang}/customers.php'),
+        'dashboard' => base_path('lang/{lang}/dashboard.php'),
+        'feedback' => base_path('lang/{lang}/feedback.php'),
+        'permissions' => base_path('lang/{lang}/permissions.php'),
+        'select2' => base_path('lang/{lang}/select2.php'),
+        'settings' => base_path('lang/{lang}/settings.php'),
+        'supervisors' => base_path('lang/{lang}/supervisors.php'),
+        'users' => base_path('lang/{lang}/users.php'),
+        'verification' => base_path('lang/{lang}/verification.php'),
         /*  The lang of generated crud will set here: Don't remove this line  */
     ],
 
     /*
-     * The paths that will scanned for translations.
+     * The paths that will scan for translations.
      */
 
     'matches' => [
