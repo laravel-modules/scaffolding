f551a606329e62bb26913c0a2d879a0edd04623f
Ahmed Fathy <aliraqi.dev@gmail.com>
2022-08-23 01:30:00 +0200

    add valid numbers middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\ReadNotificationsMiddleware::class,
+        \App\Http\Middleware\ValidNumbersMiddleware::class,
     ];
 
     /**
