7fc9b5b1228d09448e396d360cf4d2462e6b0fc6
Ahmed Fathy <fathy.dev@gmail.com>
2025-12-03 13:48:20 +0300

    add restore & force delete translations

--- lang/en/check-all.php
+++ lang/en/check-all.php
@@ -3,15 +3,29 @@
 return [
     'actions' => [
         'delete' => 'Delete Selected',
+        'restore' => 'Restore Selected',
     ],
     'messages' => [
         'deleted' => 'The :type has been selected successfully.',
+        'restored' => 'The :type has been restored successfully.',
     ],
     'dialogs' => [
         'delete' => [
             'title' => 'Warning !',
             'info' => 'Are you sure you want to delete the :type ?',
             'confirm' => 'Delete',
+            'cancel' => 'Cancel',
+        ],
+        'restore' => [
+            'title' => 'Warning !',
+            'info' => 'Are you sure you want to restore the :type ?',
+            'confirm' => 'Restore',
+            'cancel' => 'Cancel',
+        ],
+        'forceDelete' => [
+            'title' => 'Warning !',
+            'info' => 'Are you sure you want to delete the :type forever?',
+            'confirm' => 'Force Delete',
             'cancel' => 'Cancel',
         ],
     ],
